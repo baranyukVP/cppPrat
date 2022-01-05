4f3478d6b155cf89f0731f92980755d53b02a5c0
Влад Баранюк <vladbaranyuk@gmail.com>
2022-01-05 20:53:54 +0300

    added Tasks 5-7

--- cppPrat/Source.cpp
+++ cppPrat/Source.cpp
@@ -9,6 +9,8 @@ const char minutesInDegree = 60;
 const char secondsInMinute = 60;
 const char minutesInHour = 60;
 const char hoursInDay = 24;
+const float milesIn100Kilometers = 62.14;
+const float litersInGallon = 3.875;
 
 int main(void) {
 	cout << "Task 1\n";
@@ -49,6 +51,28 @@ int main(void) {
 		<< seconds / minutesInHour / secondsInMinute % hoursInDay << " hours, "
 		<< seconds / secondsInMinute % minutesInHour << " minutes, "
 		<< seconds % secondsInMinute <<" seconds";
+
+	cout << "\nTask 5\n";
+	long long worldPopulation, usaPopulation;
+	cout << "Enter the world 's population: ";
+	cin >> worldPopulation;
+	cout << "Enter the population of the US: ";
+	cin >> usaPopulation;
+	cout << "The population of the US is " << float(usaPopulation) / worldPopulation * 100 << "% of the world population.";
+
+	cout << "\nTask 6\n";
+	int miles, gallons;
+	cout << "Enter miles: ";
+	cin >> miles;
+	cout << "Enter gallons: ";
+	cin >> gallons;
+	cout << "Miles per gallon: " << float(miles) / gallons * 100;
+
+	cout << "\nTask 7\n";
+	double litresPer100Kilometers;
+	cout << "Enter liters per 100km: ";
+	cin >> litresPer100Kilometers;
+	cout << "Miles per gallon: " << milesIn100Kilometers / litresPer100Kilometers * litersInGallon;
 
 	return 0;
 };
